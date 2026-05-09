96ae048ba8278e351f9d81f684b450a9d56a4286
akine999 <akun999design@gmail.com>
2026-05-10 01:00:21 +0500

    перенес все в папку Frontend и начал делать страницу альянс

---
+++ Frontend/src/componets/MainAliance.tsx
@@ -0,0 +1,12 @@
+import './css/Main.css';
+
+function Main () {
+    return (<main className='main'>
+        <div className='mein-heading'>
+            <p className='main-subtitle'>Coalitions</p>
+            <h1 className='main-title'>РЕЙТИНГ АЛЬЯНСОВ</h1>
+        </div>
+    </main>)
+}
+
+export default Main
