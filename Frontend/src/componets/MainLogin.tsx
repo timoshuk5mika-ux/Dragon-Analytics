67ca6ce69813f8af6ca971425823b6007781ab55
akine999 <akun999design@gmail.com>
2026-05-12 22:17:46 +0500

    Начал делать страницу входа

--- Frontend/src/componets/MainLogin.tsx
+++ Frontend/src/componets/MainLogin.tsx
@@ -1,9 +1,22 @@
 
 import './css/Main.css'
+import { defaultLoginData, type LoginData } from '../data/userData';
+import { Flame } from 'lucide-react';
+
 
 function Main() {
-    return(<main>
-        
+    const user: LoginData = defaultLoginData;
+
+    return(<main className='main-login'>
+        <div className='main-login-container'>
+            <p className='main-login-subtitle'><span className='main-icon-yellow'><Flame /></span>Вход</p>
+            <h1 className='main-login-title'>Welcome back, {user.username}</h1>
+            <form className='main-login-form'>
+                <input type='text' placeholder='Username' className='main-login-input' />
+                <input type='password' placeholder='Password' className='main-login-input' />
+                <button type='submit' className='main-login-button'>Sign In</button>
+            </form>
+        </div>
     </main >)
 }
 
